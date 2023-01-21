a4dd6b9e76795efffb006608d58d9918554d0474
Devikanair4 <123161117+Devikanair4@users.noreply.github.com>
2023-01-21 20:04:51 +0530

    Add files via upload

---
+++ Practicals/Hello.java
@@ -0,0 +1,12 @@
+class My {
+	public static void display() {
+		System.out.println("Hey!");
+	}
+}
+
+class Hello {
+	public static void main(String args[]) {
+		System.out.println("This is my first program");
+		My.display();
+	}
+}
