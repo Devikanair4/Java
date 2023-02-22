71b21b67edf1c5cbd57f5338d59d3c5bb231961e
Devikanair4 <123161117+Devikanair4@users.noreply.github.com>
2023-02-22 12:55:45 +0530

    Update SortDescending.java

--- Assignments/SortDescending.java
+++ Assignments/SortDescending.java
@@ -1,15 +1,15 @@
-import java.util.Arrays;
+import java.util.Arrays; //importing for sorting
 import java.util.Collections;
 
 public class SortDescending {
     
     public static void main(String[] args) {
         
-        Integer[] originalArray = {5, 2, 9, 3, 6, 1, 8, 4, 7};
+        Integer[] originalArray = {5, 2, 9, 3, 6, 1, 8, 4, 7}; //defining array
         
-        Arrays.sort(originalArray, Collections.reverseOrder());
+        Arrays.sort(originalArray, Collections.reverseOrder()); //sorting array in descending order
         
-        System.out.println(Arrays.toString(originalArray));
+        System.out.println(Arrays.toString(originalArray)); //printing the sorted array
         
     }
     
