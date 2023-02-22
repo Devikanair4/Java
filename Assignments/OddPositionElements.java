108e5e3977250b0e05844315fc1d188c9f213ed6
Devikanair4 <123161117+Devikanair4@users.noreply.github.com>
2023-02-22 12:51:44 +0530

    Update OddPositionElements.java

--- Assignments/OddPositionElements.java
+++ Assignments/OddPositionElements.java
@@ -1,13 +1,12 @@
-public class OddPositionElements {
+public class OddPositionElements { //initializing class 
     
     public static void main(String[] args) {
         
-        int[] originalArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] originalArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; //defining array
         
         for (int i = 0; i < originalArray.length; i++) {
-            if (i % 2 != 0) {
-                System.out.println(originalArray[i]);
-            }
+            if (i % 2 != 0) { //for odd positions
+                System.out.println(originalArray[i]); //printing numbers in the odd position            }
         }
         
     }
