c896f2a762827d4ace038f3bce1f7583c43e3edd
Devikanair4 <123161117+Devikanair4@users.noreply.github.com>
2023-02-22 12:50:09 +0530

    Update MatrixOperations.java

--- Assignments/MatrixOperations.java
+++ Assignments/MatrixOperations.java
@@ -2,8 +2,9 @@ public class MatrixOperations {
     
     public static void main(String[] args) {
         
-        int[][] matrix1 = { {1, 2, 3}, {4, 5, 6}, {7, 8, 9} };
-        int[][] matrix2 = { {10, 11, 12}, {13, 14, 15}, {16, 17, 18} };
+        // defining arrays
+        int matrix1[][] = { {1, 2, 3}, {4, 5, 6}, {7, 8, 9} }; 
+        int matrix2[][] = { {10, 11, 12}, {13, 14, 15}, {16, 17, 18} };
         
         int rows1 = matrix1.length;
         int columns1 = matrix1[0].length;
@@ -14,7 +15,7 @@ public class MatrixOperations {
         
         if (rows1 == rows2 && columns1 == columns2) {
             int[][] sumMatrix = new int[rows1][columns1];
-            for (int i = 0; i < rows1; i++) {
+            for (int i = 0; i < rows1; i++) { //adding the two matrices and computing the sum
                 for (int j = 0; j < columns1; j++) {
                     sumMatrix[i][j] = matrix1[i][j] + matrix2[i][j];
                 }
@@ -22,7 +23,7 @@ public class MatrixOperations {
             System.out.println("Matrix addition:");
             for (int i = 0; i < rows1; i++) {
                 for (int j = 0; j < columns1; j++) {
-                    System.out.print(sumMatrix[i][j] + " ");
+                    System.out.print(sumMatrix[i][j] + " "); //printing the sum matrix
                 }
                 System.out.println();
             }
@@ -36,7 +37,7 @@ public class MatrixOperations {
         if (columns1 == rows2) {
             int[][] productMatrix = new int[rows1][columns2];
             for (int i = 0; i < rows1; i++) {
-                for (int j = 0; j < columns2; j++) {
+                for (int j = 0; j < columns2; j++) { //multiply the two matrices and computing the product
                     int sum = 0;
                     for (int k = 0; k < columns1; k++) {
                         sum += matrix1[i][k] * matrix2[k][j];
@@ -47,7 +48,7 @@ public class MatrixOperations {
             System.out.println("Matrix multiplication:");
             for (int i = 0; i < rows1; i++) {
                 for (int j = 0; j < columns2; j++) {
-                    System.out.print(productMatrix[i][j] + " ");
+                    System.out.print(productMatrix[i][j] + " "); //printing the product matrix
                 }
                 System.out.println();
             }
