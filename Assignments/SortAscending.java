91ad3f00db76d39ba752e75c91c5be9439a9bd1a
Devikanair4 <123161117+Devikanair4@users.noreply.github.com>
2023-02-22 12:54:20 +0530

    Update SortAscending.java

--- Assignments/SortAscending.java
+++ Assignments/SortAscending.java
@@ -1,14 +1,14 @@
-import java.util.Arrays;
+import java.util.Arrays; //imporing arrays for sorting
 
 public class SortAscending {
     
     public static void main(String[] args) {
         
-        int[] originalArray = {5, 2, 9, 3, 6, 1, 8, 4, 7};
+        int[] originalArray = {5, 2, 9, 3, 6, 1, 8, 4, 7}; //defining the array
         
-        Arrays.sort(originalArray);
+        Arrays.sort(originalArray); //sorting into ascending order
         
-        System.out.println(Arrays.toString(originalArray));
+        System.out.println(Arrays.toString(originalArray)); //printing sorted array
         
     }
     
