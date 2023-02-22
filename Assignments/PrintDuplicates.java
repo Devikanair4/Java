a576518500d2438d09f6d43ad8392edf781cda31
Devikanair4 <123161117+Devikanair4@users.noreply.github.com>
2023-02-22 12:53:05 +0530

    Update PrintDuplicates.java

--- Assignments/PrintDuplicates.java
+++ Assignments/PrintDuplicates.java
@@ -1,11 +1,11 @@
 public class PrintDuplicates {
 	public static void main(String args[]) { 
-		int array[]={2,6,3,8,2,6,4,8,9};
-		System.out.println("The Duplicate numbers are: ");
-		for (int i=0; i<array.length; i++) {
-			for (int j=i+1; j<array.length; j++) {
+		int array[]={2,6,3,8,2,6,4,8,9}; //defining array
+		System.out.println("The Duplicate numbers are: "); //print statement
+		for (int i=0; i<array.length; i++) { //traversing the array
+			for (int j=i+1; j<array.length; j++) { //comapring for duplicates
 				if (array[i]==array[j]) {
-					System.out.println(array[j]+" ");
+					System.out.println(array[j]+" "); //printing duplicate numbers
 				}
 			}
 		}
