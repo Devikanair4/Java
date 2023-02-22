3233645608bd4c04c2b66304ff7fdd748d0a3e80
Devikanair4 <123161117+Devikanair4@users.noreply.github.com>
2023-02-22 12:45:00 +0530

    Update LargestSmallestElement.java

--- Assignments/LargestSmallestElement.java
+++ Assignments/LargestSmallestElement.java
@@ -1,13 +1,13 @@
-public class LargestSmallestElement {
+public class LargestSmallestElement { //class initialization
     
-    public static void main(String[] args) {
+    public static void main(String[] args) { //main class
         
-        int[] originalArray = {5, 2, 9, 3, 6, 1, 8, 4, 7};
+        int originalArray[] = {5, 2, 9, 3, 6, 1, 8, 4, 7}; //array formation
         
-        int smallest = originalArray[0];
-        int largest = originalArray[0];
+        int smallest = originalArray[0]; //initializing variable smallest
+        int largest = originalArray[0];  //initializing variable largest
         
-        for (int i = 1; i < originalArray.length; i++) {
+        for (int i = 1; i < originalArray.length; i++) { //traversing through the array
             if (originalArray[i] < smallest) {
                 smallest = originalArray[i];
             }
