bd663dac24b9ab48f89106bfcb88ea9f5cabab17
Devikanair4 <123161117+Devikanair4@users.noreply.github.com>
2023-02-22 12:42:20 +0530

    Update EvenPositionElements.java

--- Assignments/EvenPositionElements.java
+++ Assignments/EvenPositionElements.java
@@ -1,15 +1,14 @@
-public class EvenPositionElements {
+public class EvenPositionElements { //initializing class
     
-    public static void main(String[] args) {
+    public static void main(String[] args) { //main class
         
-        int[] originalArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int originalArray[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; //creating an array
         
-        for (int i = 0; i < originalArray.length; i++) {
-            if (i % 2 == 0) {
-                System.out.println(originalArray[i]);
+        for (int i = 0; i < originalArray.length; i++) {  //traversing through the array
+            if (i % 2 == 0) {                          //even position
+                System.out.println(originalArray[i]); //printing the numbers in the even position
             }
         }
-        
     }
     
 }
